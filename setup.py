44b6e6f0d4b7624d9a522c064c3253a75e667508
Eric Leung <eric@erictleung.com>
2016-12-04 15:45:45 -0800

    Create starting setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='lalg',
+      version='0.0.1',
+      description='Python library to experiment with Linear Algebra concepts',
+      url='https://github.com/erictleung/lalg',
+      author='Eric Leung',
+      author_email='eric@erictleung.com ',
+      license='MIT',
+      packages=['lalg'],
+      zip_safe=False)
