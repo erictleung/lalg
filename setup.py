de68f167a3a11f3484e7cf55ec6c6ea903da1ba0
Eric Leung <eric@erictleung.com>
2016-12-04 15:57:00 -0800

    Add long descript. to setup and add README to dist
    Long description is added within the setup.py file with the
    long_description parameter. The README.md is added to the distribution
    using the MANIFEST.in file.

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(name='lalg',
       version='0.0.1',
       description='Python library to experiment with Linear Algebra concepts',
+      long_description=readme(),
       url='https://github.com/erictleung/lalg',
       author='Eric Leung',
       author_email='eric@erictleung.com ',
