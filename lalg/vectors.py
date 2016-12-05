406036863ed53728cdbfd151e3dcbafb9898a1c3
Eric Leung <eric@erictleung.com>
2016-12-04 17:16:58 -0800

    Update random_integers to randint in nvect()
    numpy.random.random_integers was apparently deprecated. So it was
    suggested to use the randint() method instead.

--- lalg/vectors.py
+++ lalg/vectors.py
@@ -6,4 +6,4 @@ This file contains various functions to generate vectors of all kinds
 
 
 def nvect(low=0, high=5, size=(3, 1)):
-    return np.random.random_integers(low, high, size)
+    return np.random.randint(low, high, size)
