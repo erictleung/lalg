a304f6cf0d5d5c81ee4a85b4f7b1cae3c1ad5846
Eric Leung <eric@erictleung.com>
2016-12-26 18:04:32 -0800

    Add test to test default vector length
    - Change test_nvect_default to test_nvect_default_size
    - Add test_nvect_default_range to test lower range

--- lalg/test/test_vectors.py
+++ lalg/test/test_vectors.py
@@ -1,10 +1,21 @@
 import unittest
+import numpy as np
 from lalg import *
+
 
 class TestVectors(unittest.TestCase):
 
-    def test_nvect_default(self):
+    def test_nvect_default_size(self):
+        """
+        Test default nvect() size
+        """
         self.assertEqual(nvect().size, 3)
+
+    def test_nvect_default_range(self):
+        """
+        Test default nvect() range
+        """
+        self.assertTrue(np.sum(nvect()) > 0)
 
 if __name__ == '__main__':
     unittest.main()
