1ad24124e1a25fc6425f668d1d3aaa809d40f1b7
Eric Leung <eric@erictleung.com>
2016-12-26 19:42:01 -0800

    Add docstring descriptions to existing functions

--- lalg/vectors.py
+++ lalg/vectors.py
@@ -6,8 +6,14 @@ This file contains various functions to generate vectors of all kinds
 
 
 def nvect(low=0, high=5, size=(3, 1)):
+    """
+    Create random vector
+    """
     return np.random.randint(low, high, size)
 
 
 def easy_vect(inArray, row, col):
+    """
+    Create easy vector from array and size
+    """
     return np.array(inArray).reshape(row, col)
