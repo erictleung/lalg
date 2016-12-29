4f86408acc046c3699f6b93ff5509a6ff13bce3b
Eric Leung <eric@erictleung.com>
2016-12-28 19:19:20 -0800

    Add visualization functions
    - Add viz_col_vect() to visualize 2- and 3-D vectors

---
+++ lalg/visualize.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+"""
+This file contains various functions to visualize vectors
+"""
+
+
+def viz_col_vect(colVect):
+    """
+    Visualize a 2- or 3-dimensional column vector
+    """
+    return 1
